0a612fa09fbabd76b259c6ee92e900bd01788a8e
William Stephan <william.stephan@icloud.com>
2020-04-30 15:42:09 -0500

    kenx configs

--- knexFile.js
+++ knexFile.js
@@ -1,21 +1,44 @@
 //Dependencies
 require('dotenv').config();
 
-module.exports = {
+// Global Constants
+const knexRemoteDev = false; // true to use (Jaws production mySQL instance)
+
+knexLocal = {
   development: {
-    client: `${process.env.KNEX_CLIENT}`,
+    client: `${process.env.KNEX_CLIENT_LOCAL}`,
     connection: {
-      host: `${process.env.KNEX_HOST}`,
-      user: `${process.env.KNEX_USER}`,
-      password: `${process.env.KNEX_PASSWORD}`,
-      database: `${process.env.KNEX_DATABASE}`
+      host: `${process.env.KNEX_HOST_LOCAL}`,
+      user: `${process.env.KNEX_USER_LOCAL}`,
+      password: `${process.env.KNEX_PASSWORD_LOCAL}`,
+      database: `${process.env.KNEX_DATABASE_LOCAL}`
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: `${__dirname}/orm/knex/migrations`
+      directory: `${__dirname}/orm/knex/migrations/local`
     },
     seeds: {
       directory: `${__dirname}/orm/knex/seeds`
     }
   }
 };
+knexRemote = {
+  development: {
+    client: `${process.env.KNEX_CLIENT_REMOTE}`,
+    connection: {
+      host: `${process.env.KNEX_HOST_REMOTE}`,
+      user: `${process.env.KNEX_USER_REMOTE}`,
+      password: `${process.env.KNEX_PASSWORD_REMOTE}`,
+      database: `${process.env.KNEX_DATABASE_REMOTE}`
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/orm/knex/migrations/remote`
+    },
+    seeds: {
+      directory: `${__dirname}/orm/knex/seeds`
+    }
+  }
+};
+
+knexRemoteDev ? module.exports = knexRemote : module.exports = knexLocal;
