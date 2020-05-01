a80432b8e9e42671235f2ee1d228a38e2e0e5bf3
William Stephan <william.stephan@icloud.com>
2020-04-30 19:25:31 -0500

    data test

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,29 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
+import AircraftList from "./components/AircraftList"
+
 import './App.css';
 
 
 function App() {
   return (
     <div className="background">
-    <div className="App">
-      <header className="App-header">
-        <Nav />
-        <Jumbotron>
-          <h2>FlightMX</h2>
-          <p>by the A-Team! We pity the foo...</p>
-          <Button>Bootstrap Button</Button>
-        </Jumbotron>
-        <img src={background} className="App-logo" alt="logo" />
-      </header>
-      <Footer />
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <Jumbotron>
+            <h2>FlightMX</h2>
+            <p>by the A-Team! We pity the foo...</p>
+            <Button>Bootstrap Button</Button>
+          </Jumbotron>
+          <AircraftList />
+          {/* <img src={background} className="App-logo" alt="logo" /> */}
+        </header>
+        <Footer />
       </div>
-      </div>
-      
+    </div>
+
   );
 }
 
