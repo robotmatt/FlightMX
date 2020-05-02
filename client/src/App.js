2f67191f14adcf966090c8ea902d8175f728a595
Jack Lawless <Lawless.jackm@gmail.com>
2020-05-02 10:13:57 -0500

    updated aircraft.js file and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import background from './assets/images/sr71.jpg';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
-import AircraftList from "./components/AircraftList"
-
+import AircraftList from "./components/pages/AircraftList"
 import './App.css';
 
 
 function App() {
   return (
-    <div className="background">
-      <div className="App">
-        <header className="App-header">
-          <Nav />
-          <Jumbotron>
-            <h2>FlightMX</h2>
-            <p>by the A-Team! We pity the foo...</p>
-            <Button>Bootstrap Button</Button>
-          </Jumbotron>
-          <AircraftList />
-          {/* <img src={background} className="App-logo" alt="logo" /> */}
-        </header>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={AircraftList} />
+          {/* <Route exact path="/saved" component={SaveBooks} />
+          <Route exact path="/saved/:id" component={SaveBooks} />
+          <Route component={NoMatch} />  */}
+        </Switch>
         <Footer />
       </div>
-    </div>
-
+    </Router>
   );
 }
 
---
+++ client/src/components/pages/AircraftList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class AircraftList extends Component {
+    constructor(props) {
+        super(props)
+    this.state = {
+        aircrafts: [],
+    }}
+
+    componentDidMount(){
+        axios.get(`/aircraft`).then(res => {
+            // console.log(res);
+            this.setState({ aircrafts: res.data }); 
+            console.log(Object.keys(this.state.aircrafts[0]))
+        })
+        .catch(err => console.log(err))
+    }
+
+    renderTableData(){
+    return this.state.aircrafts.map((aircraft, index) => {
+    return(
+    <tr key={aircraft._id}>
+        <td>{index +1}</td>
+        <td>{aircraft.model}</td>
+        <td>{aircraft.year}</td>
+        <td>{aircraft.tail_number}</td>
+    </tr>
+    )
+    })
+    }
+
+    renderTableHeader() {
+        let header = Object.keys(this.state.aircrafts[0])
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+        }
+
+
+    render() {
+        return (
+           <div>
+              <h1 id='title'>Aircraft List</h1>
+              <table id='aircrafts'>
+                 <tbody>
+                    {/* <tr>{this.renderTableHeader()}</tr> */}
+                    {this.renderTableData()}
+                 </tbody>
+              </table>
+           </div>
+        )
+     }
+}
+
+export default AircraftList
