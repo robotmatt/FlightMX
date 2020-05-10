51980753ddbcb091a49bf3aa5548748eea44cd42
Matt Martin <matt@incgn.to>
2020-05-10 15:59:47 -0500

    added stub about page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Error from "./components/Error"
 import Home from "./components/Home"
 import Aircraft from "./pages/Aircraft"
 import Logbook from "./pages/Logbook"
+import About from "./pages/About"
 import Login from "./components/Login"
 import Logout from "./components/Logout"
 import './App.css';
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/logbook/:aircraftID?" component={Logbook} />
           <Route exact path="/aircraft/:aircraftID?" component={Aircraft} />
+          <Route exact path="/about" component={About}/>
           <Route exact path="/login" component={Login} />
           <Route exact path="/logout" component={Logout} />
           <Route component={Error} />
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  state = {
+   
+  };
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default About;
