689f86cea0b94c6c91fb65c3703d915ded76a576
Matt Martin <matt@incgn.to>
2020-05-16 09:25:42 -0500

    more server changes

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 app.use(passport.initialize());
@@ -29,6 +31,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 // Serve up static routes
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.use('*', express.static('client/build'));
 }
 
 app.post('/register', function (req, res) {
@@ -62,7 +65,7 @@ app.use('/api/user', userRouter);
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+});
 
 // mongodb connection init 
 if (process.env.MONGODB_URI) { // Heroku
@@ -70,7 +73,10 @@ if (process.env.MONGODB_URI) { // Heroku
 } else if (mongodbRemoteDev) { // remote dev
     mongoose.connect(process.env.MONGODB_REMOTE);
 } else { // local dev
-    mongoose.connect(process.env.MONGODB_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(process.env.MONGODB_LOCAL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 }
 
 app.listen(PORT, function () {
